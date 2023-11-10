a95ee5ba225535263f23eda5b60d0d4230e04d54
Abrimo2020 <abrishmok@gmail.com>
2023-11-10 12:02:25 +0300

    Add project files.

---
+++ LandManagementSystem/Controllers/LandController.cs
@@ -0,0 +1,23 @@
+﻿using LandManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandManagementSystem.Controllers
+{
+    public class LandController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    // Retrieve a list of land parcels from your data source
+        //    List<LandModel> landParcels = GetLandParcelDataFromSource();
+
+        //    return View(landParcels);
+        //}
+        //// Add actions for adding, editing, and deleting land parcels
+
+        //private List<LandModel> GetLandParcelDataFromSource()
+        //{
+        //    // Retrieve land parcel data from your data source (e.g., database)
+        //    // Convert the data to a list of LandModel objects and return it
+        //}
+    }
+}
---
+++ LandManagementSystem/Models/LandModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LandManagementSystem.Models
+{
+    public class LandModel
+    {
+        public string ParcelNumber { get; set; }
+        public string Owner { get; set; }
+        public string Location { get; set; }
+        // Add more properties as per your requirements
+    }
+}
